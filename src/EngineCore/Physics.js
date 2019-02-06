19c24df54f9d69ddeb38783224c418f914a7a4e3
Andriy <kaminskyy.andriy@gmail.com>
2019-02-06 09:40:41 +0200

    Added impulse response

--- src/EngineCore/Physics.js
+++ src/EngineCore/Physics.js
@@ -22,6 +22,45 @@ function resolveCollision(s1, s2, collisionInfo) {
   if (mPositionalCorrectionFlag) {
     posiotionalCorrection(s1, s2, collisionInfo);
   }
+  const n = collisionInfo.getNormal();
+  const v1 = s1.mVelocity;
+  const v2 = s2.mVelocity;
+  const relativeVelocity = v2.subtract(v1);
+
+  //relative velocity in normal direction
+  const rVelocityInNormal = relativeVelocity.dot(n);
+
+
+  if (rVelocityInNormal > 0) {
+      //already moving apart
+      return;
+  }
+
+  const newRestitution = Math.min(s1.mRestitution, s2.mRestitution);
+  const newFriction = Math.min(s1.mFriction, s2.mFriction);
+
+  // impulse along normal
+  const jN = -(1 + newRestitution) * rVelocityInNormal / (s1.mInvMass + s2.mInvMass);
+
+  const impulse = n.scale(jN);
+
+  s1.mVelocity = s1.mVelocity.subtract(impulse.scale(s1.mInvMass));
+  s2.mVelocity = s2.mVelocity.add(impulse.scale(s2.mInvMass));
+
+  const tangent = relativeVelocity
+    .subtract(n.scale(relativeVelocity.dot(n)))
+    .normalize()
+    .scale(-1);
+
+  //impulse along tangent
+  let jT = -(1 + newRestitution) * relativeVelocity.dot(tangent) * newFriction  / (s1.mInvMass + s2.mInvMass);
+
+  if (jT > jN) jT = jN;
+
+  const tImpulse = tangent.scale(jT);
+
+  s1.mVelocity = s1.mVelocity.subtract(tImpulse.scale(s1.mInvMass));
+  s2.mVelocity = s2.mVelocity.add(tImpulse.scale(s2.mInvMass));
 }
 
 function posiotionalCorrection(s1, s2, collisionInfo) {
